b6ba93e912fbd823bfd590189e057aa9021d56cc
MexL <madiajijah7@gmail.com>
2023-01-30 21:14:29 +0800

    update to 1.0.2

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
-import { API_URL } from '@env';
+import Constants from 'expo-constants';
 
-const http = (token) => {
+const http = token => {
   const headers = {};
   if (token) {
     headers.authorization = `Bearer ${token}`;
   }
   const instance = axios.create({
-    baseURL: API_URL,
+    baseURL: Constants.expoConfig.extra.API_URL || process.env.API_URL,
     headers
   });
   return instance;
